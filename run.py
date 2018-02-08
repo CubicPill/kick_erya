d71d06cab2eaf6ab2d2e54c4a0baf8f0903c6a48
CubicPill <huazhengchang97@gmail.com>
2018-02-08 22:38:15 +0800

    Initial commit

---
+++ run.py
@@ -0,0 +1,20 @@
+def get_enc(clazzId: str, userid: str, jobid: str, objectId: str, playingTime: int, duration: int,
+            clipTime: str) -> str:
+    """
+    :rtype: str
+    :param clazzId: 
+    :param userid: 
+    :param jobid: 
+    :param objectId: 
+    :param playingTime: 
+    :param duration: 
+    :param clipTime: 
+    :return: enc string
+    """
+    import hashlib
+    _string = '[{cid}][{uid}][{jid}][{oid}][{pt}][d_yHJ!$pdA~5][{d}][{ct}]' \
+        .format(cid=clazzId, uid=userid, jid=jobid, oid=objectId, pt=playingTime * 1000,
+                d=duration * 1000, ct=clipTime)
+    md5 = hashlib.md5()
+    md5.update(_string.encode('utf-8'))
+    return md5.hexdigest()
