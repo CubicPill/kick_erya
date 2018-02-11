fe252501ac9826158bd42c1de37c19abb4d51799
CubicPill <huazhengchang97@gmail.com>
2018-02-11 23:37:43 +0800

    login code

--- auth.py
+++ auth.py
@@ -2,12 +2,25 @@ import requests
 from PIL import Image
 
 LOGIN_URL = 'http://passport2.chaoxing.com/login'
+CAPTCHA_URL = 'http://passport2.chaoxing.com/num/code'
 
 
-def erya_login(username, password):
+def erya_login(username, password, captcha_mode: int = 0):
     session = requests.session()
-    image = Image.frombytes(mode='RGB', size=(123, 40), data=session.get(LOGIN_URL).content)
-    # captcha recognition
+    if captcha_mode == 1:
+        image = Image.frombytes(mode='RGB', size=(123, 40), data=session.get(CAPTCHA_URL).content)
+        # captcha recognition
+        raise NotImplementedError
+    elif captcha_mode == 0:
+        pass
+    else:
+        raise ValueError('Unrecognized captcha mode %d' % captcha_mode)
+    data = {
+        'uname': username,
+        'password': password,
+        'numcode': '',
+    }
+    session.post('http://passport2.chaoxing.com/login', data=data)
     return session.cookies.get_dict()
 
 
