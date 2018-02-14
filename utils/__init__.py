303ad53243136676dd8cdb5e723a09fa92e5fe02
CubicPill <huazhengchang97@gmail.com>
2018-02-14 17:03:14 +0800

    move get_enc

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,9 @@
-def get_enc(clazzId: str, userid: str, jobid: str, objectId: str, playingTime: int, duration: int,
-            clipTime: str) -> str:
+import hashlib
+
+SALT = 'd_yHJ!$pdA~5'
+
+
+def get_enc(clazzId: int, userid: int, jobid: int, objectId: str, playingTime: int, duration: int) -> str:
     """
     get the enc field in update requests
     restored from decompiling the flash player
@@ -10,12 +14,11 @@ def get_enc(clazzId: str, userid: str, jobid: str, objectId: str, playingTime: i
     :param objectId:
     :param playingTime:
     :param duration:
-    :param clipTime:
     :return: enc string
     """
-    _string = '[{cid}][{uid}][{jid}][{oid}][{pt}][d_yHJ!$pdA~5][{d}][{ct}]' \
+    _string = '[{cid}][{uid}][{jid}][{oid}][{pt}][{salt}][{d}][{ct}]' \
         .format(cid=clazzId, uid=userid, jid=jobid, oid=objectId, pt=playingTime * 1000,
-                d=duration * 1000, ct=clipTime)
+                d=duration * 1000, ct='0_%d' % duration, salt=SALT)
     md5 = hashlib.md5()
     md5.update(_string.encode('utf-8'))
     return md5.hexdigest()
